a51681e692fe19c48e6a119ba86702854b26c506
Dinh Duong <jenny.180820@gmail.com>
2025-05-30 13:47:39 +0700

    feat(repository): add UserRepository

---
+++ src/main/java/com/fourstars/greenstore/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.fourstars.greenstore.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fourstars.greenstore.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+    Optional<User> findByNameOrEmail(String name, String email);
+
+    Optional<User> findByName(String name);
+
+    Boolean existsByName(String name);
+
+    Boolean existsByEmail(String email);
+
+}
