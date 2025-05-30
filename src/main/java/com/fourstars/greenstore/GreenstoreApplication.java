d5824387274fe7a9c40ab08eb340d8245a7214bc
Dinh Duong <jenny.180820@gmail.com>
2025-05-30 13:20:41 +0700

    feat(all): init project

---
+++ src/main/java/com/fourstars/greenstore/GreenstoreApplication.java
@@ -0,0 +1,13 @@
+package com.fourstars.greenstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fourstars/greenstore/GreenstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourstars.greenstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
