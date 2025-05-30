54512cb56334630f6f77b523fdf50a52aa6b2d93
Dinh Duong <jenny.180820@gmail.com>
2025-05-30 13:43:34 +0700

    feat(repository): add OrderRepository

---
+++ src/main/java/com/fourstars/greenstore/repository/OrderRepository.java
@@ -0,0 +1,17 @@
+package com.fourstars.greenstore.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.fourstars.greenstore.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query(value = "select * from orders where user_id = ?1 order by order_id desc", nativeQuery = true)
+    List<Order> findOrderByUserId(Long id);
+
+}
