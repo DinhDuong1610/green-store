b3f1d68c356b2888363888f6d4299fc2e4b644c3
Dinh Duong <jenny.180820@gmail.com>
2025-08-08 02:35:26 +0700

    feat(data): add dataseeder

--- src/main/java/com/fourstars/greenstore/entities/User.java
+++ src/main/java/com/fourstars/greenstore/entities/User.java
@@ -34,7 +34,7 @@ public class User implements Serializable {
 	private Date registerDate;
 	private Boolean status;
 
-	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "userId"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
 
 	private Collection<Role> roles;
